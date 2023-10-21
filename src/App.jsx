ec025b4303a227146edb0b4d1ffc99c6a736d97f
Mobinshahidi <mowbinsh@gmail.com>
2023-10-22 00:45:39 +0330

    main

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import TodoFormWrapper from './Components/TodoFormWrapper';
+import './App.css';
+
+function App() {
+	return (
+		
+		<div className='App'>
+			<TodoFormWrapper/>
+		</div>
+		
+	);
+}
+
+export default App;
